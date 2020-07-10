2b50a870cacae5e980418636a0f3dc95a1e17c1c
Артур Камалов <KamalovA@iMac-Bravedevelopers.local>
2020-07-10 14:52:18 +0400

    compress images

--- landing/gulpfile.js
+++ landing/gulpfile.js
@@ -8,31 +8,32 @@ sass.compiler = require('node-sass');
 
 
 const rules = {
-    output: {
-        filename: "index.js"
-    },
-    mode: "production",
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                "presets" : [
-                  ["@babel/preset-env"]
-                ]
-              }
-            }
+  output: {
+    filename: "index.js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "presets": [
+              ["@babel/preset-env"]
+            ]
           }
-        ]
-}}
+        }
+      }
+    ]
+  }
+}
 
-gulp.task('pug', function(){
-    return gulp.src('frontend/*.pug')
+gulp.task('pug', function () {
+  return gulp.src('frontend/*.pug')
     .pipe(pug({
-        pretty: false
+      pretty: false
     }))
     .pipe(gulp.dest('prod'))
 })
@@ -46,29 +47,29 @@ gulp.task('sass', function () {
 
 gulp.task('fonts', function () {
   return gulp.src('frontend/fonts/*.ttf')
-         .pipe(gulp.dest('prod/fonts'));
+    .pipe(gulp.dest('prod/fonts'));
 });
 
 gulp.task('img', function () {
-    return gulp.src('frontend/img/*.{jpg,png,svg}')
-        //    .pipe(tinypng({
-        //      key: '7nLKFgKtc4KKybP4TqVD2DxGm02nBNBP',
-        //      log: true
-        //  }))
-         .pipe(gulp.dest('prod/img'));
-         
-  });
-gulp.task('js', function(){
-    return gulp.src('frontend/js/*.js')
+  return gulp.src('frontend/img/*.{jpg,png,svg}')
+    .pipe(tinypng({
+      key: '7nLKFgKtc4KKybP4TqVD2DxGm02nBNBP',
+      log: true
+    }))
+    .pipe(gulp.dest('prod/img'));
+
+});
+gulp.task('js', function () {
+  return gulp.src('frontend/js/*.js')
     .pipe(webpack(rules))
     .pipe(gulp.dest("prod/js"))
 })
-   
- 
+
+
 gulp.task('watch', function () {
-    gulp.watch('frontend/sass/**/*.sass', gulp.series("sass"));
-    gulp.watch('frontend/*.pug', gulp.series("pug"));
-    gulp.watch('frontend/img/*.{jpg,png,svg}', gulp.series("img"));
-    gulp.watch('frontend/js/*.js', gulp.series("js"));
-    
+  gulp.watch('frontend/sass/**/*.sass', gulp.series("sass"));
+  gulp.watch('frontend/*.pug', gulp.series("pug"));
+  gulp.watch('frontend/img/*.{jpg,png,svg}', gulp.series("img"));
+  gulp.watch('frontend/js/*.js', gulp.series("js"));
+
 });
