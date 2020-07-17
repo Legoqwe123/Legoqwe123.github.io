035ff1164f1078d0e1e637d76e0fe293786e99eb
Артур Камалов <KamalovA@iMac-Bravedevelopers.local>
2020-07-17 18:04:11 +0400

    add insurance

---
+++ insurance/build/precache-manifest.661127c052555fbbc4828981e5702512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319c846ca590360da1a32a4404ce55fe",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c4e7088515604adc8e2d",
+    "url": "/react-app/static/css/2.5b8945e9.chunk.css"
+  },
+  {
+    "revision": "c4e7088515604adc8e2d",
+    "url": "/react-app/static/js/2.d457443e.chunk.js"
+  },
+  {
+    "revision": "7c12c184850d63cf3f8e594125287504",
+    "url": "/react-app/static/js/2.d457443e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38bc49a233019ffa862f",
+    "url": "/react-app/static/js/main.adaad024.chunk.js"
+  },
+  {
+    "revision": "e9356ebc68f7e902a8af",
+    "url": "/react-app/static/js/runtime-main.6d4c5f7e.js"
+  }
+]);
