2839cf6cafa1dc05b97d63142a80a0e42d358233
Legoqwe <bill-137@mail.ru>
2020-07-22 01:59:48 +0400

    add

--- lalal/js/mail.js
+++ lalal/js/mail.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("heelo");
+  const $form = document.querySelector("#contacts-form");
+
+  $form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = new FormData($form);
+
+    const response = await fetch("../smart.php", {
+      method: "POST",
+      body: "username=2",
+    });
+
+    console.log(data);
+  });
 });
