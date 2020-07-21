c3fe6f80466c7a0d6bfe2896e784a2e6ff4a148b
Legoqwe <bill-137@mail.ru>
2020-07-22 01:54:50 +0400

    add

--- TCM/js/main.js
+++ TCM/js/main.js
@@ -1,92 +1,65 @@
-$(function() {
+$(function () {
+  let data = $(".container").data("page");
 
-   let data = $('.container').data('page');
-  
-   $('.menu-btn').on('click', function () {  // Открываем меню 
- 
-      $('.modal-burger').show(500)
-   });
+  $(".menu-btn").on("click", function () {
+    // Открываем меню
 
-   $('.modal-close__btn').on('click', function () { //  закрываем  меню
-      $('.modal-burger').hide(500)
+    $(".modal-burger").show(500);
   });
 
-  
-  $('.modal-message__button').on('click', function () { //  закрываем блок успешной отправки сообщения на сервер
-   
-    $('.modal-message').fadeOut(500)
+  $(".modal-close__btn").on("click", function () {
+    //  закрываем  меню
+    $(".modal-burger").hide(500);
+  });
 
+  $(".modal-message__button").on("click", function () {
+    //  закрываем блок успешной отправки сообщения на сервер
+
+    $(".modal-message").fadeOut(500);
+  });
+
+  $(`a[href="${data}.php"]`).addClass("menu-active"); // Выделяем цветом ссылки на страницах которых находится пользователь
+
+  /* Скрипт валидации*/
+  $("#index-form").validate({
+    rules: {
+      username: "required",
+
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+
+    messages: {
+      username: "Пожалуйста укажите имя",
+      email: {
+        required: "Введите ваш e-mail",
+        email: "Неправильно введен адрес почты",
+      },
+    },
+  });
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    if (
+      $("label.error").length === false ||
+      $("label.error").is(":visible") === false
+    ) {
+      $.ajax({
+        type: "POST",
+        url: "smart.php",
+        data: $(this).serialize(),
+      }).done(function () {
+        $("form").find("input").val("");
+
+        $(".modal-message").fadeIn(500);
+
+        $("form").trigger("reset");
+      });
+    }
+
+    return false;
+  });
 });
-
-
-  $(`a[href="${data}.php"]`).addClass('menu-active') // Выделяем цветом ссылки на страницах которых находится пользователь
-
-
-    /* Скрипт валидации*/
-    $('#index-form').validate({  
-            
-      
-
-      rules: {
-          username: 'required', 
-
-          email: {
-              required: true ,
-              email: true
-          }
-
-      } ,
-
-          messages:{
-              username: "Пожалуйста укажите имя",
-              email: {
-                  required: "Введите ваш e-mail",
-                  email: "Неправильно введен адрес почты"
-              }
-          }
-
-      
-});
-
-
-
- 
-    $('form').submit(function(e){
-   
-        e.preventDefault();
-
-        if ( $( "label.error" ).length===false || $( "label.error" ).is(":visible")===false) {
-
-            $.ajax({
-           
-                type: "POST",
-                url: 'smart.php',
-                data: $(this).serialize()
-            
-             }).done(function(){
-              $('form').find('input').val('');
-          
-             
-              $('.modal-message').fadeIn(500);
-          
-              $('form').trigger('reset')
-            
-             });
-            
-            }
-     
-      
-     
-       return false;
-    }   
-    
-    
-    )
-    
-
-
-
-   
-
-
-})
